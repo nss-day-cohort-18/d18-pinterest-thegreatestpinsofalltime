c50c7c9de105242af3a896da2f409fe3f801fa90
“Aaron <barfootaaron@yahoo.com>
2017-03-01 14:04:00 -0600

    idk

--- app/controllers/PinListCtrl.js
+++ app/controllers/PinListCtrl.js
@@ -1,23 +1,23 @@
-// "use strict";
+"use strict";
 
-// app.controller("PinListCtrl", function($scope, PinFactory, AuthFactory, SearchTermData, $routeParams) {
+app.controller("PinListCtrl", function($scope, PinFactory, AuthFactory, SearchTermData, $routeParams) {
 
-// 	$scope.searchText = SearchTermData;
-//    let user = AuthFactory.getUser();
+	$scope.searchText = SearchTermData;
+   let user = AuthFactory.getUser();
 
-// 	PinFactory.getAllPins(user)
-// 	.then( function(pinList) {
-// 		$scope.songs = pinList;
-// 	});
+	PinFactory.getAllPins(user)
+	.then( function(pinList) {
+		$scope.songs = pinList;
+	});
 
-// 	$scope.pinDelete = function(pinId) {
-//       console.log("delete this song", pinId);
-//       PinFactory.deletePin(pinId)
-//       .then( function(response) {
-//          PinFactory.getAllPins(user).then( function(List) {
-//             $scope.pins = pinList;
-//          });
-//       });
-//    };
+	$scope.pinDelete = function(pinId) {
+      console.log("delete this song", pinId);
+      PinFactory.deletePin(pinId)
+      .then( function(response) {
+         PinFactory.getAllPins(user).then( function(pinList) {
+            $scope.pins = pinList;
+         });
+      });
+   };
 
-// });
+});
